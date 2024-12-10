58988f9321e731635a6abf8137f97f00dccbb8fa
MMDSour <soor.mmd81@gmail.com>
2024-12-10 15:12:51 +0330

    init: add user model

---
+++ src/app/models/user/user.model.ts
@@ -0,0 +1,36 @@
+import { FormControl } from '@angular/forms';
+
+export interface addUserForm {
+  firstName: FormControl <string>;
+  lastName: FormControl <string>;
+  nationalCode: FormControl <number>;
+  phoneNumber: FormControl <number>;
+  username: FormControl <string>;
+  password: FormControl <string>;
+  isAdmin: FormControl <userRole>;
+}
+
+export interface editUserForm {
+  firstName: FormControl <string>;
+  lastName: FormControl <string>;
+  nationalCode: FormControl <number>;
+  phoneNumber: FormControl <number>;
+  password: FormControl <string>;
+  isAdmin: FormControl <userRole>;
+}
+
+
+export interface User {
+  firstName: string;
+  lastName: string;
+  nationalCode:number;
+  phoneNumber: number;
+  username: string;
+  password: string;
+  isAdmin: userRole;
+}
+
+export enum userRole {
+  NORMAL = 0,
+  ADMIN = 1
+}
