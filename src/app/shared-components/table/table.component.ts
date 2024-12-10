7ca49e08a1d23c0577e3f169f406392e6387953e
MMDSour <soor.mmd81@gmail.com>
2024-12-10 15:32:27 +0330

    init: add table component

---
+++ src/app/shared-components/table/table.component.ts
@@ -0,0 +1,13 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'table',
+  imports: [],
+  templateUrl: './table.component.html',
+  standalone: true,
+  styleUrl: './table.component.scss'
+})
+
+export class TableComponent {
+
+}
