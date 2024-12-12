a3b3ac1cd601b0cd685288b4a10d7bee5fc654a7
MMDSour <soor.mmd81@gmail.com>
2024-12-12 18:28:52 +0330

    feat: Implement YesNo pipe for Admin field

---
+++ src/app/shared/pipes/yes-no.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  standalone: true,
+  name: 'yesNo'
+})
+export class YesNoPipe implements PipeTransform {
+  transform(value: number): string {
+    if (value === 1) {
+      return 'Yes';
+    } else if (value === 0) {
+      return 'No';
+    } else {
+      return 'Invalid';
+    }
+  }
+}
